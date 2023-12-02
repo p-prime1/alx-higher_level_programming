5570dd9ec974e3869c7d4a5b5b3fca59e4de17c2
p-prime1 <preciousnwaka95@gmail.com>
2023-12-02 18:03:33 +0100

    Added the 0-add.py file without the module

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for tens in range(10):
-    for unit in range(tens + 1, 10):
+    for unit in range(tens + 1, 9):
         print("{:02d}, ".format(tens * 10 + unit), end='')
+print("{}".format(tens * 10 + (unit + 1)))
