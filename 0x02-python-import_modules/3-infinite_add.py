8be88abead0effa8689e203d82680295e661e7e3
p-prime1 <preciousnwaka95@gmail.com>
2023-12-03 17:46:01 +0100

    Infinte add numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) < 1:
+        print("{}".format(0))
+    else:
+        j = 1
+        i = 1
+        while j != len(argv):
+            i += int(argv[j])
+            j += 1
+        print("{}".format(i))
