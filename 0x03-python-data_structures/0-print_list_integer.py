80f522d1fbe7d7269d8117069ecf1ee35c7b64e8
p-prime1 <preciousnwaka95@gmail.com>
2023-12-05 20:47:27 +0100

    Changed the condition in the while loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@ def print_list_integer(my_list=[]):
     "Prints all integers in of list"
     a = len(my_list) - 1
     i = 0
-    while i != a:
+    while i <= a:
         print("{:d}".format(my_list[i]))
         i += 1
