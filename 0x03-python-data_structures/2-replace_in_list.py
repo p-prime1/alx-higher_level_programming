ddd30b4f281927fd39083899d6c830b2033c0766
p-prime1 <preciousnwaka95@gmail.com>
2023-12-05 22:21:00 +0100

    D

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    "Replaces an element of a list at a specific position"
+    a = len(my_list) - 1
+    if idx > a or idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
