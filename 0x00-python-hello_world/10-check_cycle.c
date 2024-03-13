50ae45f848de078d9b62440948db809d6f864ed2
p-prime1 <preciousnwaka95@gmail.com>
2024-03-13 20:26:17 +0100

    Fixed the infinite loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks for cycle in a singly linked list
+ * @list: List to be to be checked
+ * Return: Returns 0 if there is no cycle or 1 if a cycle exist and 0 on error
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	hare = list;
+	tortoise = list;
+	if (list == NULL)
+		return (-1);
+	while (hare != NULL && hare != NULL)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
+			return (1);
+	}
+	return (0);
+}
