bdc18263b4a87dc67ef36d7531bed78f8a17ac2f
p-prime1 <preciousnwaka95@gmail.com>
2024-03-18 12:40:07 +0100

    a

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """MY addition funciton
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+        """
+    return (a + b)
