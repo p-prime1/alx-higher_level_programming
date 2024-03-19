26d384dfc106be95588690a4cee6db6d43671302
p-prime1 <preciousnwaka95@gmail.com>
2024-03-19 15:29:46 +0100

    Replaces an elements in a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Retrieves an element from a list at an index"""
+    new_list = list()
+    new_list = my_list[:]
+    if (idx > (len(new_list) - 1) or idx < 0):
+        return (NONE)
+    j = 0;
+    for i in my_list:
+        if j == idx:
+            break
+        else:
+            j += 1
+    new_list[j] = element
+    return (new_list)
