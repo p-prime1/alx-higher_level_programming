e8c02bd2852afab6fc0ea402cbf589a14d07562e
p-prime1 <preciousnwaka95@gmail.com>
2024-03-19 15:03:12 +0100

    Func retrieves an element form an index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Retrieves an element from a list at an index"""
+    if (idx > (len(my_list) - 1) or idx < 0):
+        return (NONE)
+    j = 0;
+    for i in my_list:
+        if j == idx:
+            break
+        else:
+            j += 1
+    return (my_list[j])
