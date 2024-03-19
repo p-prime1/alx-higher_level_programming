31ae6eebc943cb62c357c6451f35ab15d4443916
p-prime1 <preciousnwaka95@gmail.com>
2024-03-19 15:23:13 +0100

    Removed a semi-colon

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     """Retrieves an element from a list at an index"""
     if (idx > (len(my_list) - 1) or idx < 0):
         return (NONE)
-    j = 0;
+    j = 0
     for i in my_list:
         if j == idx:
             break
