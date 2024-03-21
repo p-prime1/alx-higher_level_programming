ee4b5e20dd061ee8a1db255bb92b5a1fa8ae2abe
p-prime1 <preciousnwaka95@gmail.com>
2024-03-21 21:12:14 +0100

    Used pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,8 +4,6 @@
 def search_replace(my_list, search, replace):
     """Searches and replaces an element in a copy of a list"""
     new_list = my_list[:]
-    ##for i in range(len(my_list)):
-      ##  new_list[i] = my_list[i]
     for i in range(len(new_list)):
         if (new_list[i] == search):
             new_list[i] = replace
