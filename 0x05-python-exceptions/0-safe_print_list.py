910b9147f12f71db477a07b45478cfefcb9bb4d2
p-prime1 <preciousnwaka95@gmail.com>
2024-04-06 21:57:33 +0100

    New func

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements in a list"""
+    i = 0
+    while(True):
+        try:
+            print(my_list[i], end="" if i != x else "\n")
+            i+=1
+            if (i == x):
+                break
+        except IndexError:
+            print("\n")
+            break
+    return (i)
+
