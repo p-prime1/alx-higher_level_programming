8cf89393a75c5dfea719cc91bbd989f0a501bdbd
p-prime1 <preciousnwaka95@gmail.com>
2024-04-06 22:30:30 +0100

    Used pycode style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     """Prints x elements in a list"""
     i = 0
-    while(True):
+    while (True):
         try:
-            print(my_list[i], end="" if i != x else "\n")
-            i+=1
+            print(my_list[i], end="" if i <= x else "\n")
+            i += 1
             if (i == x):
                 break
         except IndexError:
             print("\n")
             break
     return (i)
-
