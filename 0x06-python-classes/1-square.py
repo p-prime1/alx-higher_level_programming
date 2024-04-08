71ea40b1b2b64785fbf3b31aeadb04bcb870aabc
p-prime1 <preciousnwaka95@gmail.com>
2024-04-08 14:59:03 +0100

    Class defined with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a class square"""
+
+
+class Square:
+    """Initializes a new swuare"""
+    def __init__(self, size):
+        """Initializing the private variable size.
+        Args:
+            size (int): The size of the square.
+            """
+        self.__size = size
