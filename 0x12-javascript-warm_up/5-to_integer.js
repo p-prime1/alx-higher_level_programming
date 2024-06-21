8ff111bba18da04324aa50ffc7efa933bfd67f93
p-prime1 <preciousnwaka95@gmail.com>
2024-06-21 23:05:26 +0100

    Added new file

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+if (isNaN(num)) {
+  console.log('Not a Number');
+} else {
+  console.log(`My Number: ${num}`);
+}
