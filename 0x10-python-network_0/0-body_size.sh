1611eee41c4a34471389e4c45be10fd52b062d1f
p-prime1 <preciousnwaka95@gmail.com>
2024-07-06 00:34:57 +0100

    Added shebang and comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
-response=$(curl -sI "$1" | grep -i "Content-Length")
-echo "$response"
+#!/bin/bash
+#Sends a request to a url and returns the size of the body
+response=$(curl -sI "$1" | grep -i "Content-Length") echo "$response"
