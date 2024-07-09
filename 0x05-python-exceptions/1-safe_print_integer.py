c521ac2ed3d17f7344a02da252360f103b64a18d
p-prime1 <preciousnwaka95@gmail.com>
2024-07-09 22:28:46 +0100

    Added proper return statements

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
-        pass
+        return False
     except TypeError:
-        pass
+        return False
+    return True
