37811821ab518d143630bf50bf4a38c73322066e
p-prime1 <preciousnwaka95@gmail.com>
2024-07-09 22:03:27 +0100

    Changed the logic

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,14 @@ def safe_print_list(my_list=[], x=0):
     """Prints x elements in a list"""
     i = 0
     try:
-        while (i <= x):
+        while (i < x):
             print(my_list[i], end='')
             i += 1
-        print("\n")
     except IndexError:
         pass
     except TypeError:
         pass
-
+    except NameError:
+        pass
+    print()
+nb = safe_print_list('my_list', 50)
