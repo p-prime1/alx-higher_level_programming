7ff738df4824dbfb3a57480c9372e32f60fac794
p-prime1 <preciousnwaka95@gmail.com>
2024-07-09 22:23:43 +0100

    Added a new exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     """Prints an integer"""
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return (False)
-    return (True)
+    except (ValueError, TypeError):
+        pass
