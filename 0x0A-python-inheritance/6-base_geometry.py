ba3ff5b7781cb0914d6e774d940888d79270d854
p-prime1 <preciousnwaka95@gmail.com>
2024-08-07 23:10:11 +0100

    Added a new method to the Geometry clss

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a geometry class"""
+
+
+class BaseGeometry:
+    """An area method"""
+    def area(self):
+        """Raises an error"""
+        raise Exception("area() is not implemented")
