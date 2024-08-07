e8b0773b21308a0f41dc20be0159988ae7f05a3a
p-prime1 <preciousnwaka95@gmail.com>
2024-08-07 22:45:00 +0100

    A

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a new func that checks if an object is exactly an instance"""
+def is_same_class(obj, a_class):
+    """Function accepts two argument:
+        obj (Object): An object to be checked
+        a_class (Class): A class to check if its an instance"""
+    if type(obj) is  a_class:
+        return(True)
+    return (False)
