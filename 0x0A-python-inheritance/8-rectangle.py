5a00c05c427821de30be81adfda25b257c120047
p-prime1 <preciousnwaka95@gmail.com>
2024-08-08 21:45:45 +0100

    Added the parent class to the same file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a geometry class"""
+
+
+class BaseGeometry:
+    """An area method"""
+    def area(self):
+        """Raises an error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value if its an integer
+        Arguments:
+            name (String): Name
+            value (int): Integer to be validated
+            """
+        if value is not int:
+            raise TypeError("<name> must be an integer")
+        elif value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
 
 """A rectangle a class that inherits form BaseGeometry"""
 
