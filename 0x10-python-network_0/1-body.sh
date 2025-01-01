0b7fae8ccfdb1654a8bc763e29f995d4790e878e
p-prime1 <preciousnwaka95@gmail.com>
2025-01-02 00:29:07 +0100

    added flag to follow http redirect

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Returns the body if a status code of 200 is returned
-response_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$response_code" -eq 200 ] && curl $1
+curl -sLs "$1"
