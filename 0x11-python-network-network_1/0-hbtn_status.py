c663c5e3ea7acc57d0e04e9e86381c57a4445b74
p-prime1 <preciousnwaka95@gmail.com>
2025-01-12 14:15:48 +0100

    new file

---
+++ 0x11-python-network-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        resp = response.read()
+        print(f"""Body response:
+            - type: {type(resp)}
+            - content: {resp}
+            - utf8 content: {resp.decode('utf-8')}""")
