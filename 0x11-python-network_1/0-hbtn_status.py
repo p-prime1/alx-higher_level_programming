54c199a10f293291172be47fda7d64fa175ae239
p-prime1 <preciousnwaka95@gmail.com>
2025-01-12 17:11:58 +0100

    formatted print

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         resp = response.read()
-        print(f"""Body response:
-            - type: {type(resp)}
-            - content: {resp}
-            - utf8 content: {resp.decode('utf-8')}""")
+        print(f"Body response:\n"
+            f"\t- type: {type(resp)}\n"
+            f"\t- content: {resp}\n"
+            f"\t- utf8 content: {resp.decode('utf-8')}")
