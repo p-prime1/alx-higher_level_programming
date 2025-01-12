019cfa9f97db5959253504fd950c9a0ac35df86f
p-prime1 <preciousnwaka95@gmail.com>
2025-01-12 17:15:18 +0100

    removed indent

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         resp = response.read()
-        print(f"Body response:\n"
-            f"\t- type: {type(resp)}\n"
-            f"\t- content: {resp}\n"
-            f"\t- utf8 content: {resp.decode('utf-8')}")
+        print(f"Body response:")
+        print(f"\t- type: {type(resp)}")
+        print(f"\t- content: {resp}")
+        print(f"\t- utf8 content: {resp.decode('utf-8')}")
