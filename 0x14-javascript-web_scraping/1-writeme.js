6413114b5758359435a037ea0292bb7ff9f36f90
p-prime1 <preciousnwaka95@gmail.com>
2025-03-08 19:32:08 +0100

    writing to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+if (process.argv) {
+  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+    if (err) {
+    console.log(err);
+    }
+  });
+}
