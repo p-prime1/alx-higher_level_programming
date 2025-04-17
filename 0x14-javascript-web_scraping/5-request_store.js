3be23e2b4753a85634115246f523718870be1ca3
p-prime1 <preciousnwaka95@gmail.com>
2025-04-17 20:21:21 +0100

    fixed semistandard validation

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const request = require('request');
 
 const url = process.argv[2];
-const file_path = process.argv[3];
+const filePath = process.argv[3];
 if (url) {
-  request(url, function(error) {
+  request(url, function (error) {
     if (error) {
       console.error(error);
     }
   })
-  .pipe(fs.createWriteStream(file_path, 'utf-8', 'w'));
+    .pipe(fs.createWriteStream(filePath, 'utf-8', 'w'));
 }
